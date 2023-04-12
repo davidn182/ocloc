2bb72338008020dc5642c774e3c2b1ce4184c097
David Naranjo <davidn182@hotmail.com>
2023-04-12 18:00:14 +0200

    remove everything

--- tests/test_ocloc.py
+++ tests/test_ocloc.py
@@ -1,38 +1,5 @@
-import os
-import sys
-import pytest
-
-import obspy
+from ocloc import __version__
 
 
-from ocloc.ocloc import read_correlation_file
-
-
-class TestReadCorrelationFile:
-
-    # Test a failure.
-    def test_file_not_found(self):
-        fname = 'tests/data/badsac.sac'
-        with pytest.raises(FileNotFoundError, match='file does not exist'):
-            read_correlation_file(fname)
-
-    # 
-    def test_file_not_found(self):
-        fname = 'tests/data/KEF_O01_1413547247_100.sac'
-        tr = read_correlation_file(fname)
-
-        date = obspy.UTCDateTime(2014, 10, 17, 12, 0, 47)
-        assert tr.stats.average_date == date
-        assert tr.stats.number_of_days == 100
-        assert tr.stats.station_pair == 'KEF_O01'
-
-
-# pytest -- runs all tests
-# pytest tests/test_bla.py  -- runs this file
-# pytest tests/test_bla.py::test_a  -- runs this function
-# pytest tests/test_bla.py::TestA::test_a  -- runs this function in this class
-# flags: -svv
-# https://docs.pytest.org
-#
-# capsys - captures system output
-# tmpdir - gives temporary directory
+def test_version():
+    assert __version__ == '0.1.0'
